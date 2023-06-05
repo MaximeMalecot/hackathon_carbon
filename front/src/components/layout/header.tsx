d6c9d00590c51a9617334980895c43321718b344
julian-saleix <julian.saleix@gmail.com>
2023-06-05 20:22:33 +0200

    Adding mock login

--- front/src/components/layout/header.tsx
+++ front/src/components/layout/header.tsx
@@ -2,38 +2,40 @@ import { Link } from "react-router-dom";
 import { useAuthContext } from "../../contexts/auth.context";
 
 export default function Header() {
-    const { isConnected, data } = useAuthContext();
+    const { isConnected, data, logout } = useAuthContext();
 
     return (
         <div className="navbar bg-base-100">
             <div className="flex-1">
                 <Link to="/" className="btn btn-ghost normal-case text-xl">
                     daisyUI
+                    {isConnected ? "y" : "n"}
                 </Link>
             </div>
             <div className="flex-none">
-                <button className="btn btn-square btn-ghost">
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        className="inline-block w-5 h-5 stroke-current"
-                    >
-                        <path
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            strokeWidth="2"
-                            d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"
-                        ></path>
-                    </svg>
-                </button>
-                {isConnected ? (
-                    <p>{data!.email}</p>
-                ) : (
-                    <Link to="/login" className="btn btn-square btn-ghost">
-                        Login
-                    </Link>
-                )}
+                <ul className="menu menu-horizontal px-1">
+                    {isConnected ? (
+                        <li>
+                            <details>
+                                <summary>{data!.email}</summary>
+                                <ul className="p-2 bg-base-100">
+                                    <li>
+                                        <button onClick={logout}>Logout</button>
+                                    </li>
+                                </ul>
+                            </details>
+                        </li>
+                    ) : (
+                        <li>
+                            <Link
+                                to="/login"
+                                className="btn btn-square btn-ghost"
+                            >
+                                Login
+                            </Link>
+                        </li>
+                    )}
+                </ul>
             </div>
         </div>
     );
