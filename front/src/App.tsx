cd1cddcb21a87aee209c3d8e79a9590cb527d4a2
Clarence Potel <potel.clarence@gmail.com>
2023-06-05 23:33:00 +0200

    add router

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -4,20 +4,22 @@ import AppLayout from "./components/layout/app-layout";
 
 const Home = lazy(() => import("./pages/home"));
 const NotFound = lazy(() => import("./pages/404"));
+const Test = lazy(() => import("./pages/chartTest"));
 
 function App() {
-    return (
-        <div className="App">
-            <Suspense>
-                <Routes>
-                    <Route element={<AppLayout />}>
-                        <Route path={"/"} element={<Home />} />
-                        <Route path={"*"} element={<NotFound />} />
-                    </Route>
-                </Routes>
-            </Suspense>
-        </div>
-    );
+  return (
+    <div className="App">
+      <Suspense>
+        <Routes>
+          <Route element={<AppLayout />}>
+            <Route path={"/"} element={<Home />} />
+            <Route path={"/test"} element={<Test />} />
+            <Route path={"*"} element={<NotFound />} />
+          </Route>
+        </Routes>
+      </Suspense>
+    </div>
+  );
 }
 
 export default App;
