8898d56ea31c306210915dad9af75489555fa910
MaximeMalecot <92maximemalecot@gmail.com>
2023-06-06 18:21:08 +0200

    Added file upload, lack of path finding

--- server/src/entreprise/entreprise.controller.ts
+++ server/src/entreprise/entreprise.controller.ts
@@ -1,13 +1,35 @@
-import { Controller, Post } from "@nestjs/common";
+import {
+    Body,
+    Controller,
+    ParseFilePipeBuilder,
+    Post,
+    UploadedFile,
+    UseInterceptors,
+} from "@nestjs/common";
+import { FileInterceptor } from "@nestjs/platform-express";
 import { Roles } from "src/auth/decorators/roles.decorator";
 import { Role } from "src/users/schemas/user.schema";
+import { CreateEntrepriseDto } from "./dto/create-entreprise.dto";
 import { EntrepriseService } from "./entreprise.service";
 
 @Controller("entreprise")
 export class EntrepriseController {
     constructor(private readonly entrepriseService: EntrepriseService) {}
 
-    @Roles(Role.ADMIN)
+    @UseInterceptors(FileInterceptor("file"))
+    @Roles(Role.ENTREPRISE_EDITOR)
     @Post()
-    async createEntreprise() {}
+    async createEntreprise(
+        @Body() body: CreateEntrepriseDto,
+        @UploadedFile(
+            new ParseFilePipeBuilder()
+                .addFileTypeValidator({
+                    fileType: /(jpg)|(jpeg)|(png)/gm,
+                })
+                .build()
+        )
+        file: Express.Multer.File
+    ) {
+        console.log(file.path);
+    }
 }
