2dab0da460497a15a918fbf5882c46c426cdd768
jsaleix <julian.saleix@gmail.com>
2023-06-08 23:18:21 +0200

    Fix not same origin error

--- front/src/components/contracts/contract-item.tsx
+++ front/src/components/contracts/contract-item.tsx
@@ -29,7 +29,14 @@ export default function ContractItem({ contract }: ContractItemProps) {
     return (
         <div className="card w-96 bg-base-100 shadow-xl image-full">
             <figure>
-                <img src={DefaultFormationImage} alt="Entreprise image" />
+                <img
+                    src={
+                        entrepriseData?.image
+                            ? entrepriseData.image
+                            : DefaultFormationImage
+                    }
+                    alt="Entreprise image"
+                />
             </figure>
             <div className="card-body">
                 <h2 className="card-title capitalize">
