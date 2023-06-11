85d5efcc1393778b956f916345effec6581a2638
jsaleix <julian.saleix@gmail.com>
2023-06-11 15:57:59 +0200

    Fix post item css

--- front/src/components/posts/post.tsx
+++ front/src/components/posts/post.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { Content } from "../../interfaces/content";
 import { Entreprise } from "../../interfaces/entreprise";
 import entrepriseService from "../../services/entreprise.service";
@@ -35,7 +35,7 @@ export function Post(props: {
         return a.order - b.order;
     });
 
-    return (
+    const t = (
         <div className="card w-1/2 bg-base-100 shadow-xl border p-2 my-2">
             {entreprise && <p>Entreprise: {entreprise?.name}</p>}
             <h1 className="text-center my-3">{props.title}</h1>
@@ -62,6 +62,38 @@ export function Post(props: {
                 <div className="">
                     {new Date(props.createdAt).toLocaleDateString()}
                 </div>
+            </div>
+        </div>
+    );
+
+    const image = useCallback(() => {
+        const element = contents.find((content) => content.type === "file");
+
+        if (element) {
+            return element.data;
+        } else {
+            return "https://images.unsplash.com/photo-1686276476863-687665119815?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=80";
+        }
+    }, [contents]);
+
+    const firstContentText = useCallback(() => {
+        const element = contents.find((content) => content.type === "text");
+
+        if (element) {
+            return element.data;
+        } else {
+            return "No content text";
+        }
+    }, [contents]);
+
+    return (
+        <div className="card max-w-sm w-auto bg-base-100 shadow-xl h-80 cursor-pointer hover:scale-105 transition-5 transition">
+            <figure style={{ height: "200px", width: "100%" }}>
+                <img src={image()} alt="Shoes" />
+            </figure>
+            <div className="card-body">
+                <h2 className="text-2xl card-title">{props.title}</h2>
+                <p>{firstContentText()}</p>
             </div>
         </div>
     );
